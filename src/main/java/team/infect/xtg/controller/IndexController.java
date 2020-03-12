821dede90623b3713847f970757870cb974af529
XTG-L <xtg-l@foxmail.com>
2020-03-12 12:25:13 +0800

    update

--- src/main/java/team/infect/xtg/controller/IndexController.java
+++ src/main/java/team/infect/xtg/controller/IndexController.java
@@ -4,15 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import team.infect.xtg.bean.Directory;
+import team.infect.xtg.bean.Log;
+import team.infect.xtg.bean.Region;
+import team.infect.xtg.dao.DirectoryDAO;
+import team.infect.xtg.dao.LogDAO;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(value="index.html", method=RequestMethod.GET)
-    @ResponseBody
-    public String index(Model model){
-        model.addAttribute("name","xtg");
-        return "/index";
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String map(Model model, @RequestParam("date") String date) throws IOException, ParseException {
+        Directory directory = new Directory("src/main/log/");
+        DirectoryDAO directoryDAO = new DirectoryDAO();
+        directoryDAO.sortFiles(directory);
+        List<Log> logs = directoryDAO.getLogList(directory, date);
+        LogDAO logDAO = new LogDAO();
+        List<Region> regions = logDAO.getRegionList(logs);
+        model.addAttribute("regions", regions);
+        return "index";
     }
 }
